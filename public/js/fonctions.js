afcaaa0e9181fd94addee61852943a6f3274ab06
AmbreMychalski <ambre.mychalski@gmail.com>
2022-12-16 12:03:44 +0100

    prototype d'arbre généalogique

--- public/js/fonctions.js
+++ public/js/fonctions.js
@@ -240,6 +240,79 @@ function rechercherScientifique(objet, callback){
   });
 }
 
+function rechercherDoctoralSudent(scientistName, increment){
+  increment -= 1;
+  decodeURIComponent(scientistName);
+  scientistName = scientistName.replaceAll('(', '\\(');
+  scientistName = scientistName.replaceAll(')', '\\)');
+  scientistName = scientistName.replaceAll(',', '\\,');
+  scientistName = scientistName.replaceAll(' ', '_');
+  console.log(scientistName);
+  var requete = `
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX dc: <http://purl.org/dc/elements/1.1/>
+        PREFIX : <http://dbpedia.org/resource/>
+        PREFIX dbpedia2: <http://dbpedia.org/property/>
+        PREFIX dbpedia: <http://dbpedia.org/>
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        \n
+        SELECT GROUP_CONCAT(DISTINCT ?discipline;separator =";") AS ?disciplines 
+        GROUP_CONCAT(DISTINCT ?doctoralStudent;separator =";") AS ?doctoralStudents
+        ?name 
+        ?description 
+        ?date WHERE {
+            :${scientistName} rdfs:label ?name.
+            OPTIONAL{:${scientistName} dbo:academicDiscipline ?discipline}
+            OPTIONAL{:${scientistName} dbo:birthDate ?date}
+            OPTIONAL{:${scientistName} dbo:abstract ?description}
+            OPTIONAL{:${scientistName} dbo:doctoralStudent ?doctoralStudent}
+            FILTER(langMatches(lang(?description),"EN"))
+            FILTER(langMatches(lang(?name),"EN"))
+        }`;
+
+  // Encodage de l'URL à transmettre à DBPedia
+  var url_base = "http://dbpedia.org/sparql/";
+  $(document).ready(function () {
+    $.ajax({
+      //L'URL de la requête 
+      url: url_base,
+
+      //La méthode d'envoi (type de requête)
+      method: "GET",
+
+      //Le format de réponse attendu
+      dataType: "json",
+      data: { query: requete },
+      beforeSend: afficherChargement($("#zone-resultats-recherche"), "Chargement")
+    })
+
+      /*Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done().
+        On peut par exemple convertir cette réponse en chaine JSON et insérer
+        cette chaine dans un div id="res"
+      */
+      .done(function (response) {
+        // let data = (response);
+        afficherDoctorant(response, increment, scientistName);
+      })
+
+      /* Ce code sera exécuté en cas d'échec - L'erreur est passée à fail()
+        On peut afficher les informations relatives à la requête et à l'erreur 
+      */
+      .fail(function (error) {
+        alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+      })
+
+      // Ce code sera exécuté que la requête soit un succès ou un échec
+      .always(function () {
+        //alert("Requête effectuée");
+      });
+  });
+}
+
 function rechercherInventeur(sujet, callback){
   var requete = `PREFIX owl: <http://www.w3.org/2002/07/owl#>
                           PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
--- public/js/scientist.js
+++ public/js/scientist.js
@@ -165,6 +165,50 @@ function afficherScientist(response)
             tr.appendChild(td);
             tbody.appendChild(tr);
         }
+    }   
+    rechercherDoctoralSudent(response.results.bindings[0].name.value, 5);  
+    const ul = document.querySelector('#genetree ul'); 
+    const li = document.createElement('li');
+    li.setAttribute("id", response.results.bindings[0].name.value.replaceAll(' ', '_'))
+    ul.appendChild(li);
+    const a = document.createElement('a');
+    a.innerHTML = response.results.bindings[0].name.value;
+    li.appendChild(a);
+    const ul2 = document.createElement('ul');
+    li.appendChild(ul2);
+}
+
+function afficherDoctorant(response, increment, parentName){
+    console.log(response)
+
+    if(response.results.bindings[0].doctoralStudents.value != ''){
+        var data = response.results.bindings[0].doctoralStudents.value.split(';');
+        // Récupérez l'élément <tbody>
+        //const tbody = document.querySelector('#genealogie tbody');
+        const ul = document.querySelector('#'+parentName+' ul');
+        console.log(parentName);
+        // Insérez les données dans le tableau
+        for (const row of data) {
+            /* const tr = document.createElement('tr');
+            const td = document.createElement('td');
+            const a = document.createElement("a");
+            console.log(row);
+            a.href = "/scientist/"+row.replace('http://dbpedia.org/resource/', '');
+            a.innerHTML = row.replace('http://dbpedia.org/resource/', '').replaceAll('_', ' ');
+            td.appendChild(a);
+            tr.appendChild(td);
+            tbody.appendChild(tr); */
+            const li = document.createElement('li');
+            li.setAttribute("id", row.replace('http://dbpedia.org/resource/', '').replaceAll(' ', '_'))
+            ul.appendChild(li);
+            const a = document.createElement('a');
+            a.innerHTML = row.replace('http://dbpedia.org/resource/', '').replaceAll('_', ' ');
+            li.appendChild(a);
+            const ul2 = document.createElement('ul');
+            li.appendChild(ul2);
+            if(increment>0){
+                rechercherDoctoralSudent(row.replace('http://dbpedia.org/resource/', ''), increment);
+            }
+        }
     }
-    
 }
